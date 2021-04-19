096d75bf3a1ab21f82c96ac709d85bc39253f29c
LSaldyt <lucassaldyt@gmail.com>
2021-04-18 23:16:20 -0500

    Add data sample collection code

---
+++ arduino/combined_collect/combined_collect.ino
@@ -0,0 +1,132 @@
+#include "BluetoothSerial.h"
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+#include <NewPing.h>
+#define TRIGGER_PIN  4  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN     5  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+
+Adafruit_MPU6050 mpu;
+
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+BluetoothSerial SerialBT;
+
+void setup() {
+  Serial.begin(115200);
+  SerialBT.begin("ESP32test"); //Bluetooth device name
+  Serial.println("The device started, now you can pair it with bluetooth!");
+  // Try to initialize!
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("MPU6050 Found!");
+
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  Serial.print("Accelerometer range set to: ");
+  switch (mpu.getAccelerometerRange()) {
+  case MPU6050_RANGE_2_G:
+    Serial.println("+-2G");
+    break;
+  case MPU6050_RANGE_4_G:
+    Serial.println("+-4G");
+    break;
+  case MPU6050_RANGE_8_G:
+    Serial.println("+-8G");
+    break;
+  case MPU6050_RANGE_16_G:
+    Serial.println("+-16G");
+    break;
+  }
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  Serial.print("Gyro range set to: ");
+  switch (mpu.getGyroRange()) {
+  case MPU6050_RANGE_250_DEG:
+    Serial.println("+- 250 deg/s");
+    break;
+  case MPU6050_RANGE_500_DEG:
+    Serial.println("+- 500 deg/s");
+    break;
+  case MPU6050_RANGE_1000_DEG:
+    Serial.println("+- 1000 deg/s");
+    break;
+  case MPU6050_RANGE_2000_DEG:
+    Serial.println("+- 2000 deg/s");
+    break;
+  }
+
+  mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
+  Serial.print("Filter bandwidth set to: ");
+  switch (mpu.getFilterBandwidth()) {
+  case MPU6050_BAND_260_HZ:
+    Serial.println("260 Hz");
+    break;
+  case MPU6050_BAND_184_HZ:
+    Serial.println("184 Hz");
+    break;
+  case MPU6050_BAND_94_HZ:
+    Serial.println("94 Hz");
+    break;
+  case MPU6050_BAND_44_HZ:
+    Serial.println("44 Hz");
+    break;
+  case MPU6050_BAND_21_HZ:
+    Serial.println("21 Hz");
+    break;
+  case MPU6050_BAND_10_HZ:
+    Serial.println("10 Hz");
+    break;
+  case MPU6050_BAND_5_HZ:
+    Serial.println("5 Hz");
+    break;
+  }
+  Serial.println("");
+  delay(100);
+}
+
+void comma() {
+  SerialBT.write(0x2c);
+}
+
+void BT_float(float f_Temp) {
+  char *c_Data = ( char* ) &f_Temp;
+  for( char c_Index = 0 ; c_Index < sizeof( float ) ; SerialBT.write( c_Data[ c_Index++ ] ) );
+}
+
+void BT_log(float f) {
+  BT_float(f);
+  comma();
+}
+
+void loop() {
+  delay(50);                      // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+  sensors_event_t a, g, temp;     // Get an IMU ping
+  mpu.getEvent(&a, &g, &temp);
+  unsigned int uS = sonar.ping(); // Send ping, get ping time in microseconds (uS).
+  Serial.print("Ping: ");
+  float cm = sonar.convert_cm(uS);
+  Serial.print(cm); // Convert ping time to distance and print result (0 = outside set distance range, no ping echo)
+  Serial.println("cm");
+  BT_log(cm);
+
+
+  BT_log(a.acceleration.x);
+  BT_log(a.acceleration.y);
+  BT_log(a.acceleration.z);
+  BT_log(g.gyro.x);
+  BT_log(g.gyro.y);
+  BT_log(g.gyro.z);
+  BT_float(temp.temperature);
+  Serial.println("");
+  SerialBT.write(0x0a);
+  delay(500);
+}
--- collect/collect.py
+++ collect/collect.py
@@ -1,15 +1,13 @@
 import json, datetime
 import bluetooth
+import struct
 from time import sleep
 
-address = '3C:61:05:3D:EB:2A'
-
-def main():
+def initialize(address=''):
     service = bluetooth.find_service(address=address)
 
     if not service:
-        print(f'Could not find bluetooth device: {address}')
-        return
+        raise ValueError(f'Could not find bluetooth device: {address}')
 
     first = service[0]
     port = first['port']
@@ -19,13 +17,45 @@ def main():
     socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
     socket.connect((host, port))
     socket.settimeout(0.1)
-    try:
-        while True:
+    return socket
+
+
+def extract(buff):
+    result = []
+    if b'\n' in buff:
+        chunks = buff.split(b'\n')
+        buff = chunks[-1]
+        for chunk in chunks:
+            parts = chunk.split(b',')
             try:
-                socket.send(input())
-                print(socket.recv(1024))
-            except bluetooth.btcommon.BluetoothError:
-                pass
+                raw = [struct.unpack('f', x)[0]
+                       for x in parts if len(x) > 0]
+                if raw:
+                    result.append(raw)
+            except Exception as e:
+                print(e)
+                print(parts)
+                print([len(p) for p in parts])
+    return result, buff
+
+
+def main():
+    headers = 'cm,accel_x,accel_y,accel_z,gyro_x,gyro_y,gyro_z,temp'
+    buff = b''
+    socket = initialize(address='3C:61:05:3D:EB:2A')
+    try:
+        with open('raw_data.csv', 'w') as outfile:
+            outfile.write(headers)
+            while True:
+                try:
+                    buff += socket.recv(1024)
+                    result, buff = extract(buff)
+                    for line in result:
+                        print(line)
+                        outfile.write(','.join(map(str, line)) + '\n')
+                    sleep(1)
+                except bluetooth.btcommon.BluetoothError:
+                    pass
     finally:
         socket.close()
 
