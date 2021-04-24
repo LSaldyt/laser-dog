44dcc73a823df19837c6890dd4f7c906126034ac
LSaldyt <lucassaldyt@gmail.com>
2021-04-24 14:02:08 -0500

    Add a few comments

--- real_world/basic_classifier.py
+++ real_world/basic_classifier.py
@@ -13,9 +13,9 @@ class Net(nn.Module):
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(1886544, 120)
+        self.fc1 = nn.Linear(1886544, 120) # 1886544 established based on image size
         self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 1)
+        self.fc3 = nn.Linear(84, 1) # 1 output for binary classification task
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
@@ -23,16 +23,15 @@ class Net(nn.Module):
         x = torch.flatten(x, 1)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = torch.sigmoid(self.fc3(x))
+        x = torch.sigmoid(self.fc3(x)) # For binary classification only
         return x
 
 def main():
     net = Net()
 
-    # criterion = nn.CrossEntropyLoss()
+    # For binary classification, use BCELoss (needs replacing for regression or multi-class)
     criterion = nn.BCELoss()
-    # optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-    # optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=0.9)
+    # For batch training
     optimizer = optim.AdamW(net.parameters(), lr=0.001)
 
     dataset = LaserDataset()
@@ -45,13 +44,11 @@ def main():
     for epoch in range(100):
         total_loss = 0.0
         for i, (inputs, label) in enumerate(train_dataloader):
-            print(f'Item: {i}\r', end='')
+            print(f'Batch: {i}\r', end='')
 
             optimizer.zero_grad()
-
             outputs = net(inputs)
             loss = criterion(outputs, label)
-            print(loss)
             loss.backward()
             optimizer.step()
             total_loss += loss.item()
--- real_world/dataset.py
+++ real_world/dataset.py
@@ -12,13 +12,16 @@ import warnings
 warnings.filterwarnings("ignore")
 
 class LaserDataset(Dataset):
-
+    '''
+    A basic dataset of the laser images, optionally providing laser on/off, as well as theta/distance values
+    '''
     def __init__(self, csv_file='labels.csv', root_dir='laser_images/', transform=None, kind='classifier'):
         self.labels = pd.read_csv(csv_file)
         self.root_dir = root_dir
         self.transform = transform
 
         # Filter corrupt images ahead of time
+        # Takes a while, but makes training smoother
         print('Filtering corrupt images: ')
         bad = []
         for idx in range(len(self.labels)):
@@ -40,17 +43,21 @@ class LaserDataset(Dataset):
         image_name = os.path.join(self.root_dir, self.labels.iloc[idx, 0].strip())
         image = torch.from_numpy(io.imread(image_name))
         image = torch.movedim(image, 2, 0).float()
-        laser = self.labels.iloc[idx, 4:5]
-        laser = torch.from_numpy(np.array(laser, dtype=np.int16))
-        laser = laser.float()
-        reg = self.labels.iloc[idx, 2:4]
-        reg = torch.from_numpy(np.array(reg, dtype=np.float32))
-        reg = reg.float()
-        sample = dict(image=image, laser=laser, reg=reg)
 
-        if self.transform:
-            sample = self.transform(sample)
-        return image, laser
+        '''
+        Generate the correct labels depending on the task.
+          classifier = laser on/off
+          regressor  = theta & distance
+        '''
+        if kind == 'classifier':
+            laser = self.labels.iloc[idx, 4:5]
+            laser = torch.from_numpy(np.array(laser, dtype=np.int16))
+            label = laser.float()
+        elif kind = 'regressor':
+            reg = self.labels.iloc[idx, 2:4]
+            reg = torch.from_numpy(np.array(reg, dtype=np.float32))
+            label = reg.float()
+        return image, label
 
 if __name__ == '__main__':
     dataset = LaserDataset()
